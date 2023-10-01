9a94e0fe68ec8a06af5242ece14e1b7636a4c7cc
Hiromu-Watanabe <hiro_mu0166@icloud.com>
2023-10-02 00:53:58 +0900

    feat: バリデーション変更 wip summary: エッジ検出によるバリデーション

--- src/components/views/CameraComponent.vue
+++ src/components/views/CameraComponent.vue
@@ -7,17 +7,25 @@
       class="license-guide"
     />
   </div>
-  <button @click="snapPhoto">Snap Photo</button>
-  <p>{{ validResult ? "正常に撮影されました" : "免許証を枠内に正しく収めて撮影してください" }}</p>
+  <button
+    @click="snapPhoto"
+    :disabled="!validResult"
+    :class="{ active_button: validResult, disable_button: !validResult }"
+  >
+    Snap Photo
+  </button>
+  <p>{{ validResult ? "" : "免許証を枠内に正しく収めて撮影してください" }}</p>
   <canvas ref="canvasElement" width="640" height="480"></canvas>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 
 const videoElement = ref<HTMLVideoElement | null>(null);
 const canvasElement = ref<HTMLCanvasElement | null>(null);
 const validResult = ref<boolean>(false);
+const licenseEdgeData = ref<ImageData | null>(null);
+let validationInterval: number | null = null;
 
 /**
  * 1.カメラ映像を取得
@@ -37,21 +45,57 @@ onMounted(async () => {
       console.error("Error accessing the camera", err);
     }
   }
+
+  // 撮影ガイド画像のエッジ取得
+  await getLicenseEdgeData();
+
+  // 500ミリ秒ごとにバリデーションを実行
+  validationInterval = window.setInterval(() => {
+    validResult.value = validateImage();
+  }, 500);
 });
+
+onBeforeUnmount(() => {
+  if (validationInterval !== null) {
+    clearInterval(validationInterval);
+  }
+});
+
+// 事前に免許証イラストからエッジデータを取得
+const getLicenseEdgeData = async () => {
+  const licenseImage = new Image();
+  licenseImage.src = "src/assets/transparent_license.png";
+
+  await new Promise<void>((resolve, reject) => {
+    licenseImage.onload = function () {
+      const canvas = document.createElement("canvas");
+      canvas.width = licenseImage.width;
+      canvas.height = licenseImage.height;
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.drawImage(licenseImage, 0, 0);
+        const licenseImageData = ctx.getImageData(0, 0, licenseImage.width, licenseImage.height);
+        if (licenseImageData) {
+          licenseEdgeData.value = sobelFilter(licenseImageData);
+        }
+      }
+      resolve();
+    };
+
+    licenseImage.onerror = function () {
+      console.error("Failed to load the image.");
+      reject(new Error("Failed to load the image."));
+    };
+  });
+};
 
 /** スクショ機能(ボタン押下時のvideoの状態を画像としてcanvasに描画) */
 const snapPhoto = () => {
+  console.log("ボタンイベント発火");
   if (videoElement.value && canvasElement.value) {
     const context = canvasElement.value.getContext("2d");
     if (context) {
       context.drawImage(videoElement.value, 0, 0, 640, 480);
-      if (validateImage()) {
-        validResult.value = true;
-        console.log("Validation passed!");
-      } else {
-        validResult.value = false;
-        console.log("Validation failed!");
-      }
     }
   }
 };
@@ -59,36 +103,110 @@ const snapPhoto = () => {
 /**
  * バリデーション機能
  * - ガイドの枠内に免許証が正しく収められているか判定
- * - 現状は色味(白系統が占める割合が70%以上)で判定
  */
 const validateImage = () => {
-  if (!canvasElement.value) return false;
+  if (videoElement.value) {
+    // 一時的なcanvasを作成してバリデーションを行う
+    const tempCanvas = document.createElement("canvas");
+    tempCanvas.width = 640;
+    tempCanvas.height = 480;
+    const context = tempCanvas.getContext("2d");
+    if (context) {
+      context.drawImage(videoElement.value, 0, 0, 640, 480);
 
-  const context = canvasElement.value.getContext("2d");
-  if (!context) return false;
+      // ガイド画像の位置とサイズを取得
+      const guideWidth = tempCanvas.width * 0.7; // 70% of canvas width
+      const guideHeight = (guideWidth * 480) / 640; // Maintain aspect ratio
+      const guideX = (tempCanvas.width - guideWidth) / 2;
+      const guideY = (tempCanvas.height - guideHeight) / 2;
 
-  const imageData = context.getImageData(0, 0, canvasElement.value.width, canvasElement.value.height);
-  const data = imageData.data;
+      // ガイドの位置とサイズをもとに、canvasから該当する部分の画像データを取得
+      const imageData = context.getImageData(guideX, guideY, guideWidth, guideHeight);
+      const edges = sobelFilter(imageData);
 
-  let whitePixelCount = 0;
-  const totalPixels = data.length / 4;
+      // // エッジの数をカウント
+      // const edgeCount = Array.from(edges.data).filter((value, index) => index % 4 === 0 && value > 128).length;
 
-  for (let i = 0; i < data.length; i += 4) {
-    const r = data[i];
-    const g = data[i + 1];
-    const b = data[i + 2];
+      if (edges && licenseEdgeData.value) {
+        // 免許証のエッジデータと比較
+        const similarity = calculateSimilarity(edges, licenseEdgeData.value);
+        console.log(similarity);
+        // エッジの形状が90%以上類似していればtrueを返す
+        return similarity > 0.9;
+      }
+    }
+  }
+  return false;
+};
 
-    // TODO: 免許証が正しく枠内に収まったかどうかのバリデーションの仕方を探す => 「色味で判定はない方法」かつ「OpenCVなどの重い処理を行わず軽量に動くこと」を必要条件
-    // 白色または白系統の色を検出 (一旦 RGB 値がそれぞれ 200 以上としているが部屋のライトの色味や光量によって左右されてしまう)
-    if (r > 200 && g > 200 && b > 200) {
-      whitePixelCount++;
+/**
+ * ソーベルフィルタ
+ * 画像内の色の変化が急な部分 (エッジ) を検出
+ */
+const sobelFilter = (imageData: ImageData): ImageData => {
+  const width = imageData.width;
+  const height = imageData.height;
+  const output = new ImageData(width, height);
+  const sobelX = [
+    [-1, 0, 1],
+    [-2, 0, 2],
+    [-1, 0, 1],
+  ];
+  const sobelY = [
+    [-1, -2, -1],
+    [0, 0, 0],
+    [1, 2, 1],
+  ];
+
+  for (let y = 1; y < height - 1; y++) {
+    for (let x = 1; x < width - 1; x++) {
+      let gx = 0;
+      let gy = 0;
+      for (let ky = -1; ky <= 1; ky++) {
+        for (let kx = -1; kx <= 1; kx++) {
+          const index = ((y + ky) * width + (x + kx)) * 4;
+          // Convert to grayscale using the average method
+          const pixel = imageData.data[index] + imageData.data[index + 1] + imageData.data[index + 2];
+          gx += pixel * sobelX[ky + 1][kx + 1];
+          gy += pixel * sobelY[ky + 1][kx + 1];
+        }
+      }
+      const magnitude = Math.sqrt(gx * gx + gy * gy);
+      const index = (y * width + x) * 4;
+      output.data[index] = magnitude;
+      output.data[index + 1] = magnitude;
+      output.data[index + 2] = magnitude;
+      output.data[index + 3] = 255;
     }
   }
 
-  const whitePixelRatio = whitePixelCount / totalPixels;
+  return output;
+};
 
-  // 白色の割合が 70% 以上かどうかを確認
-  return whitePixelRatio > 0.7;
+/**
+ * Calculate the cosine similarity between two vectors.
+ * @param {Array} vecA - The first vector.
+ * @param {Array} vecB - The second vector.
+ * @returns {number} The cosine similarity between vecA and vecB.
+ */
+const cosineSimilarity = (vecA: Array<number>, vecB: Array<number>) => {
+  const dotProduct = vecA.reduce((sum, a, idx) => sum + a * vecB[idx], 0);
+  const magnitudeA = Math.sqrt(vecA.reduce((sum, a) => sum + a * a, 0));
+  const magnitudeB = Math.sqrt(vecB.reduce((sum, b) => sum + b * b, 0));
+  console.log(dotProduct / (magnitudeA * magnitudeB));
+  return dotProduct / (magnitudeA * magnitudeB);
+};
+
+/**
+ * Calculate the similarity between two edge data.
+ * @param {ImageData} edgesA - The first edge data.
+ * @param {ImageData} edgesB - The second edge data (license edge data).
+ * @returns {number} The similarity between edgesA and edgesB.
+ */
+const calculateSimilarity = (edgesA: ImageData, edgesB: ImageData): number => {
+  const vecA = Array.from(edgesA.data).filter((_, idx) => idx % 4 === 0);
+  const vecB = Array.from(edgesB.data).filter((_, idx) => idx % 4 === 0);
+  return cosineSimilarity(vecA, vecB);
 };
 </script>
 
@@ -122,7 +240,7 @@ video {
 }
 
 /* Button styling */
-button {
+.active_button {
   background-color: #007bff;
   color: #fff;
   display: block;
@@ -133,10 +251,33 @@ button {
   cursor: pointer;
   transition: background-color 0.3s;
 }
-
-button:hover {
-  background-color: #0056b3;
+.disable_button {
+  background-color: #808080;
+  color: #fff;
+  display: block;
+  margin: auto;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s;
 }
+
+button {
+  background-color: #808080;
+  color: #fff;
+  display: block;
+  margin: auto;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+/* button:hover {
+  background-color: #0056b3;
+} */
 
 /* Canvas styling */
 canvas {
